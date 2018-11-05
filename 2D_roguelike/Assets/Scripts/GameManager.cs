b5e3eb6b089f81ecd7d292f0b122f10be08a941f
chocoberu <43817454+chocoberu@users.noreply.github.com>
2018-11-05 22:48:02 +0900

    유튜브 5장까지

--- 2D_roguelike/Assets/Scripts/GameManager.cs
+++ 2D_roguelike/Assets/Scripts/GameManager.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+    public static GameManager instance = null; // static은 클래스 자체에 속함 (인스턴스가 아닌)
+    public BoardManager boardScript;
+    private int level = 3;
 	// Use this for initialization
-	void Start () {
-		
+	void Awake() {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+        // GameManager는 하나만 필요하므로 2개이상 생겼으면 제거
+        DontDestroyOnLoad(gameObject);
+        // 우리가 새로운 신을 로드할 때 일반적으로 하이라키의 모든 게임 오브젝트가 소멸
+        // 하지만 GameManager는 그대로 존재해야하므로 파괴를 막음
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
 	}
-	
+	void InitGame()
+    {
+        boardScript.SetupScene(level);
+    }
 	// Update is called once per frame
 	void Update () {
 		
---
+++ 2D_roguelike/Assets/Scripts/Loarder.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// GameManager가 인스턴스화 되었는지 체크한 후
+// 그렇지 않으면 프리팹으로부터 하나 인스턴스화
+public class Loarder : MonoBehaviour {
+
+    public GameManager gameManager;
+	// Use this for initialization
+	void Awake () {
+        if (GameManager.instance == null)
+            Instantiate(gameManager);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
